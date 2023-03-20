3b151a407a8acb3262edba842c96cd79bd6b990a
Omar Elbedwehy <98553227+OmarElbedwehy@users.noreply.github.com>
2023-03-20 21:12:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,49 +4,6 @@ window.onload = () => {
   document.querySelector(".intro-text").style.opacity = "1";
 }
 
-let scroll_animation = ()=>{
-  if (window.scrollY >= document.querySelector("#features").offsetTop - 350) {
-    document.querySelectorAll(".feat").forEach((ele) => {
-      ele.classList.add("trans_feat");
-    })
-  } else {
-    document.querySelectorAll(".feat").forEach((ele) => {
-      ele.classList.remove("trans_feat");
-    })
-  }
-  if (window.scrollY >= document.querySelector("#services").offsetTop - 350) {
-    document.querySelector(".services .container").classList.add("serv_active")
-  } else {
-    document.querySelector(".services .container").classList.remove("serv_active")
-  }
-  if (window.scrollY >= document.querySelector("#portfolio").offsetTop - 350) {
-    document.querySelectorAll(".card").forEach((ele) => {
-      ele.classList.add("trans_card");
-    })
-  } else {
-    document.querySelectorAll(".card").forEach((ele) => {
-      ele.classList.remove("trans_card");
-    })
-  }
-  if (window.scrollY >= document.querySelector("#about").offsetTop - 350) {
-    document.querySelector(".about .container").classList.add("trans_about");
-  } else {
-    document.querySelector(".about .container").classList.remove("trans_about");
-  }
-  if (window.scrollY >= document.querySelector("#contact").offsetTop - 350) {
-    document.querySelector(".contact-content").style.transform = "translateX(0vw)";
-  } else {
-    document.querySelector(".contact-content").style.transform = "translateX(-80vw)";
-  }
-  if (window.scrollY >= document.querySelector("#features").offsetTop) {
-    document.querySelector(".trans_header").classList.add("trans_header_active");
-    document.querySelector(".scroll_top").style.transform = "translateX(0vw)";
-  } else {
-    document.querySelector(".trans_header").classList.remove("trans_header_active");
-    document.querySelector(".scroll_top").style.transform = "translateX(20vw)";
-  }
-}
-
 setInterval(()=>{
   window.onscroll = () => {
     if (window.matchMedia("(max-width: 1444px)").matches) {
@@ -54,7 +11,7 @@ setInterval(()=>{
         document.querySelectorAll(".feat").forEach((ele) => {
           ele.classList.add("trans_feat");
         })
-      } else {
+      }else {
         document.querySelectorAll(".feat").forEach((ele) => {
           ele.classList.remove("trans_feat");
         })
@@ -90,10 +47,8 @@ setInterval(()=>{
         document.querySelector(".trans_header").classList.remove("trans_header_active");
         document.querySelector(".scroll_top").style.transform = "translateX(20vw)";
       }
-      scroll_animation()
     }
     if (window.matchMedia("(max-width: 768px)").matches){
-      scroll_animation = function(){ };
       if (window.scrollY >= document.querySelector("#features").offsetTop){
         document.querySelector(".mobile_trans_header").classList.add("mobile_trans_header_active");
         document.querySelector(".scroll_top").style.right = "20px"
